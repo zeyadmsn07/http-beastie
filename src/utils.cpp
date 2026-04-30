54166742fb59ad2f65ae260cc40b8a0341a64977
Zeyad <zeyadmsn@aucegypt.edu>
2026-04-30 23:43:07 +0300

    fixed develop branch

--- src/utils.cpp
+++ src/utils.cpp
@@ -16,7 +16,12 @@ http::response<http::string_body> handle_request(const http::request<http::strin
     if (req.target() == "/") {
       res.result(http::status::ok);
       res.body() = "<h1 style=\"text-align: center;\">CSCE 1102</h1>";
-    } else {
+    }
+    else if (req.target() == "/Nada") {
+      res.result(http::status::ok);
+      res.body() = accessFileContent("static/Nada.html");
+    } 
+    else {
       res.result(http::status::not_found);
       res.body() = "<h1 style=\"text-align: center;\">404 Not Found</h1>";
     }
